668cb9d21ca911b823bce595d06f2323d8847457
Lena <imagine.youeself19@gmail.com>
2022-06-24 15:40:25 +0300

    решена вторая задача из дз

---
+++ Задача2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите два числа для определения максимального");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+Console.WriteLine($"Из чисел {numberA} и {numberB} число {max} является наибольшим");
