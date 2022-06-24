a4f0beb921552b636c1b1f5a5803434630962b20
Lena <imagine.youeself19@gmail.com>
2022-06-24 16:25:44 +0300

    решена задача 8

---
+++ Задача8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Напишите число для получения стоящих до него четных");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <=a) 
+{
+    Console.WriteLine(i);
+    i = i + 2;
+}
