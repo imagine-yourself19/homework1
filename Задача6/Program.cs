ba49fc8d8c567c142dff6ffb6905991cc638e391
Lena <imagine.youeself19@gmail.com>
2022-06-24 16:04:57 +0300

    решена задача 6

---
+++ Задача6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число для определения, четное оно или нет");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 ==0) {
+    Console.WriteLine($"Число {a} четное");
+    }
+else {Console.WriteLine($"Число {a} нечетное");
+}
