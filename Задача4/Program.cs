1b9623695d556177162b2fedfe96f4bb10d95ae4
Lena <imagine.youeself19@gmail.com>
2022-06-24 15:55:21 +0300

    решена задача 4

---
+++ Задача4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите три числа для определения максимального");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a < b && b < c) max = c;
+if (a > b && b < c) max = a;
+if (a < b && b > c) max = b;
+Console.WriteLine($"Из трех чисел {a} {b} {c} наибольшим является {max}");
